d2ee9a3c059df4f6420d3c2b42ee19f901a130f8
unknown <hkala042@uottawa.ca>
2023-10-29 15:13:48 -0400

    Hilaire's code added

---
+++ PORJET-SEG/app/src/test/java/User.java
@@ -0,0 +1,57 @@
+public class User {
+
+    // les attributs de l utilisateur
+
+    protected String role;
+    private String name;
+    private String username;
+    private String password;
+
+    //le constructeur
+
+    public User(String name, String username, String password) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
+
+
+    //les getteurs
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    // les setteurs
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // la methode checkPS verifie si le String passe en parametre correspond au password de l utilisateur
+
+    public boolean checkPS (String password){
+        boolean isChecked = this.password.equals(password);
+        return isChecked;
+    }
+}
